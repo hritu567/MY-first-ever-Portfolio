b42282057d07d687bc9a9ba7e88ecc0a91436210
hritu567 <68611383+hritu567@users.noreply.github.com>
2020-08-28 15:17:09 +0530

    Update SideNav.js

--- src/components/SideNav/SideNav.js
+++ src/components/SideNav/SideNav.js
@@ -4,7 +4,7 @@ import './sidenav.css';
 export const SideNav=({handleSectionClick})=>{
     return(
         <div className="sidenav">
-            <img src="https://scontent-del1-1.xx.fbcdn.net/v/t1.15752-9/109748090_1018995878532708_2843945152862440269_n.jpg?_nc_cat=100&_nc_sid=b96e70&_nc_ohc=IxN8A7Uhx6MAX_ws6FE&_nc_ht=scontent-del1-1.xx&oh=a574a4f9b73eb68636293cd3a9f6ecab&oe=5F3C0675"  className="avatar" alt="Hritushree Mog"/>
+            <img src="https://i.ibb.co/qMTG0QT/hritushree.jpg"  className="avatar" alt="Hritushree Mog"/>
             <h2 className="main-name">Hritushree Mog</h2>
             <p className="subtitle">"A Cadet Who is looking for an Opportunity in front-end"</p>
             <div className="sections-list">
